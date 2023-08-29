23365a0abce4ff8d4f9e3e6694ee1ff5fa900fc1
SwornaCode <kasfiamostafa03@gmail.com>
2023-08-29 22:42:16 +0600

    done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let [milliseconds, seconds, minutes,hours] = [0,0,0,0];
-let timeRef = document.querySelector('timer-display');
+let timerRef = document.querySelector('.timer-display');
 let int = null;
 
 document.getElementById('start-button').addEventListener('click',() => {
@@ -10,5 +10,28 @@ document.getElementById('start-button').addEventListener('click',() => {
 });
 
 function displayTimer(){
-  console.log('it works')
+  milliseconds += 10;
+  seconds = milliseconds == 1000 ? (seconds + 1) % 60 
+   : seconds;
+  minutes = seconds == 0 && milliseconds == 0 ? (minutes + 1 ) % 60 : minutes;
+  hours = minutes == 0 && seconds == 0 && milliseconds == 0 ? hours + 1 : hours;
+  milliseconds = milliseconds == 1000 ? 0 : milliseconds;
+
+  let h = String(hours).padStart(2, "0")
+  let m = String(minutes).padStart(2, "0")
+  let s = String(seconds).padStart(2, "0")
+  let ms = String(milliseconds).padStart(3, "0")
+
+  timerRef.innerHTML =`${h} : ${m} : ${s} : ${ms}`;
+
 }
+
+
+document.getElementById('pause-button').addEventListener('click', () => {
+ clearInterval(int);
+})
+document.getElementById('reset-button').addEventListener('click', () => {
+ clearInterval(int);
+ [milliseconds, seconds, minutes,hours] = [0,0,0,0];
+ timerRef.innerHTML = `00 : 00 : 00 : 000`
+});
